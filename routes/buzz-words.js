8d0d50d0a8df15034a2c95476ccc1293ea0a7cde
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-01-28 12:24:08 -1000

    cleaned up code for posting new buzzword objects

--- routes/buzz-words.js
+++ routes/buzz-words.js
@@ -2,10 +2,8 @@
 
 const saveBuzzWords = require('../save-buzz-words');
 const express = require('express');
-const bodyParser = require('body-parser');
 const router = express.Router();
-const app = express();
-
+const bodyParser = require('body-parser');
 const urlEncoded = bodyParser.urlencoded({ extended: false});
 
 // get the buzzwords from POST requests, store them as their own objects.
@@ -18,22 +16,40 @@ router.get('/', (req, res) => {
   res.send(saveBuzzWords);
 });
 
+// POST request helper:
+// validates that an object with the same buzzword does not already exist in the array.
+const foundMatch = (checkObj) => {
+  if (typeof checkObj !== 'object') {
+    throw new Error('checkObj expected an object');
+  }
+
+  let detected = false;
+
+  saveBuzzWords.forEach(buzz => {
+    if (buzz['buzzWord'] === checkObj['buzzWord']) { detected = true; }
+  });
+
+  return detected;
+}
+
 router.post('/', urlEncoded, (req, res) => {
+  let success = false;
+
+  // if an object hasn't been created yet...
+  if (!foundMatch(req.body)) {
+    success = true;
+    saveBuzzWords.push(req.body);
+  }
+
+  res.send({ "success": success });
+  return success;
+});
+
+router.put('/', urlEncoded, (req, res) => {
   let alreadyExists = false;
   let success = false;
 
-  saveBuzzWords.forEach(buzz => {
-    if (buzz['buzzWord'] === req.body['buzzWord']) {
-      alreadyExists = true;
-    }
-  });
-
-  if (!alreadyExists) {
-    alreadyExists = true; 
-    success = true;
-
-    saveBuzzWords.push(req.body);
-  }
+  
 
   res.send({ "success": success });
   return success;
